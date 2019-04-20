96e12f9a3bd52dfe1303e920d04cc635ec9570da
abcbuzhiming <abcbuzhiming@gmail.com>
2019-04-20 23:15:45 +0800

    SampleNetCoreConsoleApp

---
+++ SampleNetCoreConsoleApp/SampleNetCoreConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleNetCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello .Net Core!");
+        }
+    }
+}
