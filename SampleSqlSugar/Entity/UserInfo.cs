1de2de940ec0c7d6880a0d0aca8cd6e9483f0c77
abcbuzhiming <abcbuzhiming@gmail.com>
2019-05-22 23:41:28 +0800

    20190522 2341

---
+++ SampleSqlSugar/Entity/UserInfo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SqlSugar;
+
+namespace SampleSqlSugar.Entity
+{
+    ///<summary>
+    ///用户信息
+    ///</summary>
+    [SugarTable("user_info")]
+    public partial class UserInfo
+    {
+           public UserInfo(){
+
+
+           }
+           /// <summary>
+           /// Desc:id
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           [SugarColumn(IsPrimaryKey=true,IsIdentity=true)]
+           public int id {get;set;}
+
+           /// <summary>
+           /// Desc:介绍者id,没有为0
+           /// Default:0
+           /// Nullable:False
+           /// </summary>           
+           [SugarColumn(ColumnName="parent_id")]
+           public int parentId {get;set;}
+
+           /// <summary>
+           /// Desc:用户名
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           public string user_name {get;set;}
+
+           /// <summary>
+           /// Desc:密码
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           public string password {get;set;}
+
+           /// <summary>
+           /// Desc:昵称
+           /// Default:
+           /// Nullable:True
+           /// </summary>           
+           public string nick_name {get;set;}
+
+           /// <summary>
+           /// Desc:真实姓名
+           /// Default:
+           /// Nullable:True
+           /// </summary>           
+           public string true_name {get;set;}
+
+           /// <summary>
+           /// Desc:手机号码
+           /// Default:
+           /// Nullable:True
+           /// </summary>           
+           public string mobile_phone_number {get;set;}
+
+           /// <summary>
+           /// Desc:电子邮箱
+           /// Default:
+           /// Nullable:True
+           /// </summary>           
+           public string email {get;set;}
+
+           /// <summary>
+           /// Desc:性别：0，女;1，男
+           /// Default:
+           /// Nullable:True
+           /// </summary>           
+           public byte? sex {get;set;}
+
+           /// <summary>
+           /// Desc:创建时间
+           /// Default:CURRENT_TIMESTAMP
+           /// Nullable:False
+           /// </summary>           
+           public DateTime create_time {get;set;}
+
+           /// <summary>
+           /// Desc:更新时间
+           /// Default:CURRENT_TIMESTAMP
+           /// Nullable:False
+           /// </summary>           
+           public DateTime update_time {get;set;}
+
+    }
+}
--- SampleSqlSugar/Program.cs
+++ SampleSqlSugar/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System;
 using System.Collections.Generic;
 using SqlSugar;
+using SampleSqlSugar.Entity;
 
 
 namespace SampleSqlSugar
@@ -13,6 +14,7 @@ namespace SampleSqlSugar
         {
             Console.WriteLine("Hello  SqlSugar Core Orm!");
             test1();
+            //createEntity();
         }
 
         static void test1()
@@ -32,6 +34,7 @@ namespace SampleSqlSugar
                 //Console.WriteLine();
             };
 
+            /*
             var t12 = db.SqlQueryable<dynamic>("select * from user_info").ToPageList(1, 2);//返回动态类型
             foreach (var obj in t12)
             {
@@ -41,6 +44,39 @@ namespace SampleSqlSugar
                 }
                 Console.WriteLine();
             }
+             */
+            var t12 = db.SqlQueryable<UserInfo>("select * from user_info").ToPageList(1, 2);//返回动态类型
+            foreach (var obj in t12)
+            {
+                Console.WriteLine(obj.parentId);
+            }
+            
+        }
+
+        //生成实体类，存在缺陷，不能转换为驼峰
+        static void createEntity(){
+            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = "server=127.0.0.1;port=3306;uid=sa;pwd=lanshanTest@2018&26g;database=test1",     //连接字符串基于sqlconnection格式(不同数据库格式不同)
+                DbType = DbType.MySql,      //数据库类型
+                IsAutoCloseConnection = true,       //自动释放数据务，如果存在事务，在事务结束后释放
+                InitKeyType = InitKeyType.Attribute     //从实体特性中读取主键自增列信息
+            });
+
+            List<DbTableInfo> dbTableInfoList = db.DbMaintenance.GetTableInfoList();
+             foreach (var dbTableInfo in dbTableInfoList){
+                 //Console.WriteLine(DbTableInfo.Name);
+                 var tableName = dbTableInfo.Name;
+                 List<DbColumnInfo> dbColumnInfoList = db.DbMaintenance.GetColumnInfosByTableName(tableName);
+                 foreach(var dbColumnInfo in dbColumnInfoList){
+                     //Console.WriteLine(dbColumnInfo.DbColumnName);
+                     var columnName = dbColumnInfo.DbColumnName;
+                 }
+             }
+
+            db.MappingTables.Add("userInfo", "user_info");
+            db.MappingColumns.Add("parentId", "parent_id", "userInfo");
+            db.DbFirst.IsCreateAttribute().CreateClassFile("f:\\Demo","SampleSqlSugar.Entity");
         }
     }
 }
