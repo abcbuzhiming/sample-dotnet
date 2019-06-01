f4f0f93cca6e7156d2c13ebb456cb0fe908328ad
abcbuzhiming <abcbuzhiming@gmail.com>
2019-06-01 23:49:08 +0800

    20190601 2349

--- SampleAspNetSwagger/Controllers/SampleController.cs
+++ SampleAspNetSwagger/Controllers/SampleController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace SampleAspNetSwagger.Controllers
@@ -17,11 +18,39 @@ namespace SampleAspNetSwagger.Controllers
         /// <summary>
         /// 范例 hello.
         /// </summary>
+        /// <remarks>用户登录接口</remarks>
         [HttpGet]
         public string hello()
         {
-            
+
             return "sample hello";
+        }
+
+        /// <summary>
+        /// 以queryString的方式传参
+        /// </summary>
+        /// <remarks>演示参数附加在url上提交</remarks>
+        /// <param name="name">用户名</param>
+        /// <param name="password">密码</param>
+        [HttpGet]
+        public string query([Required]string name, [Required]string password)
+        {
+            Console.WriteLine("param:" + name + "|" + password);
+            return "sample request by query param ";
+        }
+
+        /// <summary>
+        /// 以form表单方式传参
+        /// </summary>
+        /// <remarks>演示参数以form表单的方式提交，参数名称必须加[FromForm]特性，另外可以用Consumes设定Content-Type头强制Swagger的请求头</remarks>
+        /// <param name="name">用户名</param>
+        /// <param name="password">密码</param>
+        [HttpPost]
+        [Consumes("application/x-www-form-urlencoded")]
+        public string form([Required][FromForm]string name, [Required][FromForm]string password)
+        {
+            Console.WriteLine("param:" + name + "|" + password);
+            return "sample request by form param ";
         }
     }
 }
--- SampleAspNetSwagger/README.md
+++ SampleAspNetSwagger/README.md
@@ -1,6 +1,10 @@
 # SampleAspNetSwagger
 Asp.Net Core 2.2 使用Swagger UI范例，Swagger的库是Swashbuckle.AspNetCore
 
+## 参考
+[Swagger/OpenAPI](https://docs.microsoft.com/zh-cn/aspnet/core/tutorials/web-api-help-pages-using-swagger)
+
+
 ## 要点
 * 和Java环境下的Swagger是通过自己的注解来实现功能不一样，asp.net环境下是依赖asp.net自身的功能注解来实现功能
 * 方法名上必须有HttpAction注解，比如HttpGet,HttpPost，否则启动异常
