dbd86f50648234c2d2eb0a1fe4e5b58334f77e2a
abcbuzhiming <abcbuzhiming@gmail.com>
2019-08-16 18:04:57 +0800

    20190816 1804

--- SampleAspNetLTP/LTP/Segmentation.cs
+++ SampleAspNetLTP/LTP/Segmentation.cs
@@ -16,7 +16,8 @@ namespace SampleAspNetLTP.LTP
 
         //[DllImport(@"E:/NLP/LTP/lib/segmentor.dll")]
         [DllImport(@"E:/NLP/LTP/ltp-3.4.0-SourceCode/lib/Release/segmentor.dll")]
-        public static extern int segmentor_segment_csharp(IntPtr parser,byte[] inStr,StringBuilder words);
+        public static extern int segmentor_segment_csharp(IntPtr parser,byte[] inStr,IntPtr words);
+        //public static extern int segmentor_segment_csharp(IntPtr parser,byte[] inStr,StringBuilder words);
 
         public static IntPtr parser;        //分词器的指针
 
@@ -28,13 +29,37 @@ namespace SampleAspNetLTP.LTP
         }
 
         public static string splite(string source){
-            byte[] byteUtf8 = Encoding.UTF8.GetBytes(source);
-            StringBuilder sb = new StringBuilder(source.Length * 2 + 10);
             Console.WriteLine("source:" + source);
-            segmentor_segment_csharp(parser,byteUtf8,sb);
-            Console.WriteLine("result:" + sb.ToString());
-            string resultStr = sb.ToString();
+
+            byte[] byteUtf8 = Encoding.UTF8.GetBytes(source);
+            for (int i = 0; i < byteUtf8.Length; i++)
+            {
+                Console.Write(byteUtf8[i].ToString("x2"));
+            }
+            Console.WriteLine();
+            
+            int length = byteUtf8.Length + source.Length * 4;
+            //int length = 1024;
+            IntPtr words= Marshal.AllocHGlobal(length);    //申请内存，注意内存上可能不干净，有别的数据,需要用下面的代码清0
+            for(int i=0; i < length; i++)
+            {
+                Marshal.WriteByte(words, i, 0);     //内存清0
+            }
+            
+            segmentor_segment_csharp(parser,byteUtf8,words);
+
+            string resultStr = Marshal.PtrToStringUTF8(words);
+            Marshal.FreeHGlobal(words);     //释放内存，否则会泄露
+            byte[] bytes = Encoding.UTF8.GetBytes(resultStr);
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                Console.Write(bytes[i].ToString("x2"));
+            }
+            
             return resultStr;
         }
+
+
+        
     }
 }
